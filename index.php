3ab9a1aef537ec87e99a5623871a8176b78da336
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2020-11-05 08:36:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ echo "Witam"
 <body>
 <?php
 
-$conn = new mysqli("","","","");
+$conn = new mysqli("mateusz_kus","mateusz","Strona123","mateusz_kus");
 $sql = "SELECT * FROM pracownicy";
 $wynik = mysqli_query($conn, $sql);
 
