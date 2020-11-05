a80842998581a3ee2d1a84d4edc4cc1257ea405c
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2020-11-05 08:29:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
 <?php
 echo "Witam"
+  <!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php
+
+$conn = new mysqli("","","","");
+$sql = "SELECT * FROM pracownicy";
+$wynik = mysqli_query($conn, $sql);
+
+
+    echo('<table border="1">');
+    echo('<th>Imie</th><th>zarobki</th>');
+
+    while($wiersz=mysqli_fetch_assoc($wynik)){
+        echo('<tr>');
+        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+
+
 ?>
+</body>
+</html>
