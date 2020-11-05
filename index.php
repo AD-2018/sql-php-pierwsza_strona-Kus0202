3fced496a5dfc01b6fa5cf90a17c5a3efd1021c7
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2020-11-05 09:38:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
  <h1>Zadanie 1 Mateusz Kuś</h1>
  
 <?php
-$servername = " mysql-mateusz.alwaysdata.net"
- $username = "mateusz"
- $password = "Strona123"
- $dbname = " mateusz"
+$servername = "sql2.freemysqlhosting.net"
+ $username = "sql2374586"
+ $password = "zB1!qI2!"
+ $dbname = "sql2374586"
  
 $conn = new mysqli("$servername","$username","$password","$dbname");
 if ($conn->connect_error) {
