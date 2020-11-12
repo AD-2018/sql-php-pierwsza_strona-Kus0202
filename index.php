ff6c31bd56f040563e0948ae6112037fadbbe858
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2020-11-12 08:31:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -14,10 +14,7 @@ $servername = " mysql-mateusz.alwaysdata.net"
  $dbname = "mateusz"
  
 $conn = new mysqli("$servername","$username","$password","$dbname");
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
+
  
  
  $sql = "SELECT * FROM pracownicy";
