44e766d034888f76468dcdafa1634e084a77fff2
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2020-11-12 08:52:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,35 +4,64 @@
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
- 
- <h1>Zadanie 1 Mateusz Kuś</h1>
- 
 <?php
-$servername = " mysql-mateusz.alwaysdata.net";
- $username = "mateusz";
- $password = "Strona123";
- $dbname = "mateusz";
- 
-$conn = new mysqli("$servername","$username","$password","$dbname");
+$servername = "mysql-mateusz.alwaysdata.net";
+$username = "mateusz";
+$password = "Strona123";
+$dbname = "mateusz";
 
- 
- 
- $sql = "SELECT * FROM pracownicy";
-$wynik = mysqli_query($conn, $sql);
+$conn = new mysqli ($servername, $username, $password, $dbname);
+    echo("<h3> PIERWOWZÓR </h3>");
+$sql = "SELECT * FROM pracownicy";
+    echo ("<li>".$sql."</li><br><br>");
+$result = mysqli_query($conn, $sql);
+    echo ('<table border = "1" class = "moja_tabelka">');
+    echo ("<tr><th>imie</th><th>zarobki</th><th>data_urodzenia</th><th>dzial</th></tr>");
+        while ($row = mysqli_fetch_assoc($result)) {
+                echo ('<tr>');
+                echo ('<td>'.$row["imie"].'</td><td>'.$row["zarobki"].'</td><td>'.$row["data_urodzenia"].'</td><td>'.$row["dzial"].'</td>');
+                echo ('</tr>');
+        }echo ('</table>');
 
-
-    echo('<table border="1">');
-    echo('<th>Imie1</th><th>zarobki</th>');
-
-    while($wiersz=mysqli_fetch_assoc($wynik)){
-        echo('<tr>');
-        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
-        echo('</tr>');
-    }
-
-    echo('</table>');
-
-
-?>
-</body>
+    
+     echo("<h3> ZAD 1 </h3>");
+$sql = "SELECT * FROM pracownicy where dzial =2";
+    echo ("<li>".$sql."</li><br><br>");
+$result = mysqli_query($conn, $sql);
+    echo ('<table border = "1" class = "moja_tabelka">');
+    echo ("<tr><th>imie</th><th>zarobki</th><th>data_urodzenia</th><th>dzial</th></tr>");
+        while ($row = mysqli_fetch_assoc($result)) {
+                echo ('<tr>');
+                echo ('<td>'.$row["imie"].'</td><td>'.$row["zarobki"].'</td><td>'.$row["data_urodzenia"].'</td><td>'.$row["dzial"].'</td>');
+                echo ('</tr>');
+        }echo ('</table>');
+    
+    
+       echo("<h3> ZAD 2 </h3>");
+$sql = "SELECT * FROM pracownicy where (dzial = 2 or dzial = 3)";
+    echo ("<li>".$sql."</li><br><br>");
+$result = mysqli_query($conn, $sql);
+    echo ('<table border = "1" class = "moja_tabelka">');
+    echo ("<tr><th>imie</th><th>zarobki</th><th>data_urodzenia</th><th>dzial</th></tr>");
+        while ($row = mysqli_fetch_assoc($result)) {
+                echo ('<tr>');
+                echo ('<td>'.$row["imie"].'</td><td>'.$row["zarobki"].'</td><td>'.$row["data_urodzenia"].'</td><td>'.$row["dzial"].'</td>');
+                echo ('</tr>');
+        }echo ('</table>');
+    
+    
+       echo("<h3> ZAD 3 </h3>");
+$sql = "SELECT * FROM pracownicy where zarobki<30";
+    echo ("<li>".$sql."</li><br><br>");
+$result = mysqli_query($conn, $sql);
+    echo ('<table border = "1" class = "moja_tabelka">');
+    echo ("<tr><th>imie</th><th>zarobki</th><th>data_urodzenia</th><th>dzial</th></tr>");
+        while ($row = mysqli_fetch_assoc($result)) {
+                echo ('<tr>');
+                echo ('<td>'.$row["imie"].'</td><td>'.$row["zarobki"].'</td><td>'.$row["data_urodzenia"].'</td><td>'.$row["dzial"].'</td>');
+                echo ('</tr>');
+        }echo ('</table>');
+    
+    ?>
+    </body>
 </html>
