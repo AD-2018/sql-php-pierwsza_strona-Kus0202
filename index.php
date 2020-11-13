06b60dac6264d2eeca35611b638fd4b181e850bc
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2020-11-13 11:34:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Jakub Erfurt nr1</h1>
+    <h1>Mateusz Kuś</h1>
     <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-Kus0202">github</a>
     <br>
     <a href="pracownicy_organizacja.php">Organizacja i Pracownicy</a>
