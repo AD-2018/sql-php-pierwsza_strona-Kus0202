673fb6714b9bbc4d4f1e1176fccb838e5c6ae534
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2020-11-19 10:07:15 +0100

    Update data_czas.php

--- data_czas.php
+++ data_czas.php
@@ -8,7 +8,7 @@
     <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-Kus0202">github</a>
     <div class = "nav">
         <a href="index.php">Strona Główna</a>
-        <a href="pracownicy_organizacja.php">Pracownicy Organizacja</a>
+        <a href="pracownicy_organizacja.php">Pracownicy i Organizacja</a>
         <a href="funkcje_agregujące.php">Funkcje Agregujace</a>
         <a href="data_czas.php">Data i Czas</a>
     </div>
