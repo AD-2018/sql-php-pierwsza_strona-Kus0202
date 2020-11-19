ec520b94f4682af2974b777bc0f09927a6c04532
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2020-11-19 12:15:03 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -7,7 +7,7 @@ echo("<li>".$_POST["Firstname"]);
 echo("<li>".$_POST["Lastname"]);
 echo("<li>".$_POST["City"]);
 echo("<li>".$_POST["Phone"]);
-echo("<li>".$_POST["Poscode"]);
+echo("<li>".$_POST["Postcode"]);
 echo("<ul>");
 	
 ?>
