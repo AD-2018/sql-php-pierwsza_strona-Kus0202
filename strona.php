06674e198e500d791d4a17591cc64790e71d55c9
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2020-11-19 09:19:24 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -1,9 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+
+<div class = "nav">
+        <a href="index.php">strona główna</a>
+        <br>
+        <a href="pracownicy_organizacja.php">pracownicy organizacja</a>
+        <br>
+        <a href="funkcje_agregujace.php">Funkcje Agregujace</a>
+        <br>
+        <a href="data_czas.php">Data i Czas</a>
+        <br>
+        <a href="Formularz.html">Formularze</a>
+        <br>
+        <a href="strona.php">Strona Formularza</a>
+    </div>
+
+
 <?php
 	echo("jesteś na stronie.php");
 	
 	echo("<ul>");
 	
 	echo("<li>".$_POST["firstname"]);
+	echo("<li>".$_POST["lastname"]);
+	echo("<li>".$_POST["city"]);
+	echo("<li>".$_POST["phone"]);
+	echo("<li>".$_POST["postcode"]);
 		
 	echo("<ul>");
 ?>
