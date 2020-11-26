1258eedc824386609e3da8e492f487d3cce5c90f
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2020-11-26 11:43:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -17,18 +17,7 @@
     <a href="DaneDoBazy.html">Dane Do Bazy</a>
     <a href="delete.php">Delete</a>
     <?php
-    $conn = new mysqli ($servername, $username, $password, $dbname);
-    echo("<h3> Tabela Pracownicy Organizacja </h3>");
-$sql = "SELECT * FROM pracownicy,organizacja";
-    echo ("<li>".$sql."</li><br><br>");
-$result = mysqli_query($conn, $sql);
-    echo ('<table border = "1" class = "moja_tabelka">');
-    echo ("<tr><th>imie</th><th>zarobki</th><th>data_urodzenia</th><th>dzial</th></tr>");
-        while ($row = mysqli_fetch_assoc($result)) {
-                echo ('<tr>');
-                echo ('<td>'.$row["imie"].'</td><td>'.$row["zarobki"].'</td><td>'.$row["data_urodzenia"].'</td><td>'.$row["dzial"].'</td>');
-                echo ('</tr>');
-        }echo ('</table>');
+    
     </body>
-        </html>
-        ?>
+    </html>
+      
