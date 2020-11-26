0b625ada0b36a83cc51d49731bc196eedaea5668
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2020-11-26 08:41:19 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,1 +1,29 @@
+<?php
+echo("jestes w insert.php");
+echo "<li>".$_POST['imie'];
+echo "<li>".$_POST['dzial'];
+echo "<li>".$_POST['zarobki'];
+echo "<li>".$_POST['data_urodzenia'];
 
+
+$servername = "mysql-mateusz.alwaysdata.net";
+$username = "mateusz";
+$password = "Strona123";
+$dbname = "mateusz_kus";
+
+$conn = new mysqli ($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
+       VALUES (null, '".$_POST['imie']."', '".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
+echo("<br>");
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
