54ccff6ebc04b0c83aea8e7654ca7d075281c1dc
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2020-12-03 08:05:15 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,25 +1,23 @@
-<html>
-<head>
-<link rel="stylesheet" href="style.css">
-</head>
-<body>
 <?php
-echo("Delete<br>");
 echo $_POST['id_pracownicy'];
 
-require_once("connect.php");
+require "connect.php";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
 
 $sql = "DELETE FROM pracownicy WHERE id_pracownicy=".$_POST['id_pracownicy'];
 
 echo $sql;
 
 if ($conn->query($sql) === TRUE) {
-    header('Location:https://kusmateusz.herokuapp.com.DaneDoBazy.php');
+  echo "<br>Record deleted successfully";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
 $conn->close();
 ?>
-</body>
-</html>
