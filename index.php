9f7f3029bad514caf0453a4ad1318d0e2164b18b
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2020-12-03 08:32:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <a href="funkcje_agregujące.php">Funkcje agregujące</a>
     <a href="data_czas.php">Data i Czas</a>
     <a href="formularz.html">Formularz</a>
-    <a href="DaneDoBazy.html">Dane Do Bazy</a>
+    <a href="DaneDoBazy.php">Dane Do Bazy</a>
    <?php
 require_once("connect.php");
 
