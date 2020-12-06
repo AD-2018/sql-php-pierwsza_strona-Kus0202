abec4367e3185a9a543eec81ccc1033f34f036fb
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2020-12-06 13:51:25 +0100

    Update funkcje_agregujące.php

--- funkcje_agregujące.php
+++ funkcje_agregujące.php
@@ -8,16 +8,14 @@
 </head>
 <body>
     <h1>Mateusz Kuś nr.20</h1>
-    <nav>
-        <ul>
-            <li><a href="https://https://github.com/AD-2018/sql-php-pierwsza_strona-Kus0202"</a></li>
-		<li><a href="index.php">Strona Główna</a></li>
-                <li><a href="pracownicy_organizacja.php">Pracownicy i Organizacja</a></li>
-                <li><a href="data_czas.php">Data i Czas</a></li>
-                <li><a href="formularz.html">Formularz</a></li>
-                <li><a href="DaneDoBazy.php">Dane Do Bazy</a></li>
-</ul>
-</nav>
+    
+           <a href="https://https://github.com/AD-2018/sql-php-pierwsza_strona-Kus0202"</a>
+		<a href="index.php">Strona Główna</a>
+                <a href="pracownicy_organizacja.php">Pracownicy i Organizacja</a>
+                <a href="data_czas.php">Data i Czas</a>
+                <a href="formularz.html">Formularz</a>
+               <a href="DaneDoBazy.php">Dane Do Bazy</a>
+
     </body>
 </html>
 <?php
