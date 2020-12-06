78b4c2ff8e6ba58e08d8c4713a3c049532a490eb
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2020-12-06 13:22:03 +0100

    Update data_czas.php

--- data_czas.php
+++ data_czas.php
@@ -4,13 +4,18 @@
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Mateusz Kuś nr.20</h1>
-    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-Kus0202">github</a>
-    <div class = "nav">
-        <a href="index.php">Strona Główna</a>
-        <a href="pracownicy_organizacja.php">Pracownicy i Organizacja</a>
-        <a href="funkcje_agregujące.php">Funkcje Agregujace</a>
-    </div>
+  <h1>Mateusz Kuś nr.20</h1>
+    <nav>
+        <ul>
+            <li><a href="https://https://github.com/AD-2018/sql-php-pierwsza_strona-Kus0202</a></li>
+                <br>
+		        <li><a href="index.php">Strona Główna</a></li>
+                <li><a href="pracownicy_organizacja.php">Pracownicy i Organizacja</a></li>
+                <li><a href="funkcje_agregujące.php">Funkcję Agregujące</a></li>
+                <li><a href="formularz.html">Formularz</a></li>
+                <li><a href="DaneDoBazy.php">Dane Do Bazy</a></li>
+</ul>
+</nav>
 <?php
   $servername = "mysql-mateusz.alwaysdata.net";
 $username = "mateusz";
