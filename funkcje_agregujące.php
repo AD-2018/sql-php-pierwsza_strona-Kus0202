9f2c89b42e5af2843ee13f84c39b3ac6be73b380
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2020-12-10 16:27:50 +0100

    Update funkcje_agregujące.php

--- funkcje_agregujące.php
+++ funkcje_agregujące.php
@@ -25,7 +25,6 @@ require_once("connect.php");
    echo("<h1>sum,avg,count</h1>"); 
     
 $sql ="select sum(zarobki) from pracownicy"; 
-echo("<h3>Suma zarobków wszystkich pracowników</h3>");
     echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
@@ -39,7 +38,6 @@ while($row=mysqli_fetch_assoc($result)){
 echo('</table>'); 
     
 $sql ="select sum(zarobki) from pracownicy where imie like '%a'"; 
-echo("<h3>Suma zarobków wszystkich kobiet</h3>");
     echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
@@ -53,7 +51,6 @@ while($row=mysqli_fetch_assoc($result)){
 echo('</table>'); 
         
 $sql ="select sum(zarobki) from pracownicy where imie not like '%a' and (dzial=2 or dzial=3)"; 
-echo("<h3>Suma zarobków mężczyzn pracujących w dziale 2 i 3  </h3>");
     echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
@@ -67,7 +64,6 @@ while($row=mysqli_fetch_assoc($result)){
 echo('</table>'); 
         
 $sql ="select avg(zarobki) from pracownicy where imie not like '%a'"; 
-echo("<h3>Średnia zarobków wszystkich mężczyzn </h3>");
     echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
@@ -81,7 +77,6 @@ while($row=mysqli_fetch_assoc($result)){
 echo('</table>'); 
         
 $sql ="select avg(zarobki) from pracownicy where (dzial=4)"; 
-echo("<h3>Średnia zarobków pracowników z działu 4 </h3>");
     echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
@@ -95,7 +90,6 @@ while($row=mysqli_fetch_assoc($result)){
 echo('</table>'); 
         
 $sql ="select avg(zarobki) from pracownicy where imie not like '%a' and (dzial=1 or dzial=2)"; 
-echo("<h3>Średnia zarobków mężczyzn z działu 1 i 2  </h3>");
     echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
@@ -109,7 +103,6 @@ while($row=mysqli_fetch_assoc($result)){
 echo('</table>'); 
         
 $sql ="select count(imie) from pracownicy"; 
-echo("<h3>Ilu jest wszystkich pracowników </h3>");
     echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
@@ -123,7 +116,6 @@ while($row=mysqli_fetch_assoc($result)){
 echo('</table>'); 
         
 $sql ="select count(imie) from pracownicy where imie like '%a' and (dzial=1 or dzial=3)"; 
-echo("<h3>Ilu jest wszystkich pracowników </h3>");
     echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
@@ -139,7 +131,6 @@ echo('</table>');
        echo("<h1>Group By</h1>"); 
     
 $sql ="select nazwa_dzial, sum(zarobki) from pracownicy,organizacja where id_org=dzial group by nazwa_dzial"; 
-echo("<h3>Suma zarobków w poszczególnych działach </h3>");
     echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
@@ -153,7 +144,6 @@ while($row=mysqli_fetch_assoc($result)){
 echo('</table>'); 
     
     $sql ="select nazwa_dzial, count(imie) from pracownicy,organizacja where id_org=dzial group by nazwa_dzial"; 
-echo("<h3>Ilość pracowników w poszczególnych działach</h3>");
     echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
@@ -167,7 +157,6 @@ while($row=mysqli_fetch_assoc($result)){
 echo('</table>'); 
     
     $sql ="select nazwa_dzial, avg(zarobki) from pracownicy,organizacja where id_org=dzial group by nazwa_dzial"; 
-echo("<h3>Średnie zarobków w poszczególnych działach </h3>");
     echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
@@ -181,7 +170,6 @@ while($row=mysqli_fetch_assoc($result)){
 echo('</table>'); 
         
     $sql ="select sum(zarobki), if((imie like '%a'), 'Kobiety','Mężczyźni') as 'plec' from pracownicy group by plec"; 
-echo("<h3>Suma zarobków kobiet i mężczyzn</h3>");
     echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
@@ -195,7 +183,6 @@ while($row=mysqli_fetch_assoc($result)){
 echo('</table>'); 
         
     $sql ="select avg(zarobki), if((imie like '%a'), 'Kobiety','Mężczyźni') as 'plec' from pracownicy group by plec"; 
-echo("<h3>Średnia zarobków kobiet i mężczyzn</h3>");
     echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
@@ -211,7 +198,6 @@ echo('</table>');
            echo("<h1>Having</h1>"); 
             
     $sql ="select nazwa_dzial,sum(zarobki) from pracownicy,organizacja where id_org=dzial group by nazwa_dzial having sum(zarobki)<28"; 
-echo("<h3>Suma zarobków w poszczególnych działach mniejsza od 28 </h3>"); 
        echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
@@ -225,7 +211,6 @@ while($row=mysqli_fetch_assoc($result)){
 echo('</table>'); 
                     
     $sql ="select nazwa_dzial,avg(zarobki) from pracownicy,organizacja where id_org=dzial group by nazwa_dzial having avg(zarobki)>30"; 
-echo("<h3>Średnie zarobków mężczyzn w poszczególnych działach większe od 30 </h3>"); 
        echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
@@ -239,7 +224,6 @@ while($row=mysqli_fetch_assoc($result)){
 echo('</table>'); 
      
     $sql ="select count(imie),nazwa_dzial from pracownicy,organizacja where id_org=dzial group by nazwa_dzial having count(imie)>3"; 
-echo("<h3>Ilość pracowników w poszczególnych działach większa od 3  </h3>"); 
        echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
