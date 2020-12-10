054a6a29b5b3fe0cc3e683005dea2d7db4b5a472
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2020-12-10 08:31:30 +0100

    Update DaneDoBazy.php

--- DaneDoBazy.php
+++ DaneDoBazy.php
@@ -38,15 +38,18 @@ echo("<h3>Tabela Pracowników:</h3>");
     echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
-echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>"); 
-while($row=mysqli_fetch_assoc($result)){ 
+echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th><th>Usuń Pracownika</th></tr>"); 
+while($row=mysqli_fetch_assoc($result)){    
   echo("<tr>");     
-  echo("<tr>");     
-  echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");     
-  echo("<tr>"); 
+echo('<td>'.$row['id_pracownicy'].'</td><td>'.$row['imie'].'</td><td>'.$row['dzial'].'</td><td>'.$row['zarobki'].'</td><td>'.$row['data_urodzenia'].'</td>'.
+'<td>
+<form action="delete.php" method="POST">
+<input name="id_pracownicy" value="'.$row['id_pracownicy'].'" hidden>
+<input type="submit" value="Usuń Pracownika">
+</form>
+</td>');
   echo("</tr>"); } 
 echo('</table>'); 
 ?>
 </body>
 </html>
-
