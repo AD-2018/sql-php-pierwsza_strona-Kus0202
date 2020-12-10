38bf3cb8a0d95704f88b58527fa651d8866c0f3d
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2020-12-10 08:43:16 +0100

    Update DaneDoBazy.php

--- DaneDoBazy.php
+++ DaneDoBazy.php
@@ -12,7 +12,7 @@
     <nav>
         <ul>
              <li><a href="https://github.com/AD-2018/sql-php-pierwsza_strona-Kus0202">GitHub</a></li> 
-               
+                <li><a href="index.php">Strona Główna</a></li>
 		<li><a href="pracownicy_organizacja.php">Pracownicy i Organizacja</a></li>
                 <li><a href="funkcje_agregujące.php">Funkcję Agregujące</a></li>
                 <li><a href="data_czas.php">Data i Czas</a></li>
