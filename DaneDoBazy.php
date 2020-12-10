14a3c9187167c9d6f58ccd383a09fe58226cb0c6
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2020-12-10 08:18:35 +0100

    Update DaneDoBazy.php

--- DaneDoBazy.php
+++ DaneDoBazy.php
@@ -46,21 +46,22 @@ $password = "Strona123";
 $dbname = "mateusz_kus";    
 
 <?php
-require "connect.php";
-
-$conn= new mysqli($servername,$username,$password,$dbname);  
-     
+require_once("connect.php"); 
 $sql ="select * from pracownicy"; 
 echo("<h3>Tabela Pracowników:</h3>"); 
     echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
-echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>"); 
-while($row=mysqli_fetch_assoc($result)){ 
+echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th><th>Usuń Pracownika</th></tr>"); 
+while($row=mysqli_fetch_assoc($result)){    
   echo("<tr>");     
-  echo("<tr>");     
-  echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");     
-  echo("<tr>"); 
+echo('<td>'.$row['id_pracownicy'].'</td><td>'.$row['imie'].'</td><td>'.$row['dzial'].'</td><td>'.$row['zarobki'].'</td><td>'.$row['data_urodzenia'].'</td>'.
+'<td>
+<form action="delete.php" method="POST">
+<input name="id_pracownicy" value="'.$row['id_pracownicy'].'" hidden>
+<input type="submit" value="usuń pracownika">
+</form>
+</td>');
   echo("</tr>"); } 
 echo('</table>'); 
 ?>
