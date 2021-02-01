6c2426acd7c602a65effa54f3961870d171ee1ac
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-02-01 08:46:12 +0100

    Update data_czas.php

--- pracownicy/data_czas.php
+++ pracownicy/data_czas.php
@@ -7,12 +7,12 @@
   <h1>Mateusz Kuś nr.20</h1>
     <nav>
         <ul>
-                <li><a href="https://github.com/AD-2018/sql-php-pierwsza_strona-Kus0202">GitHub</a></li> 
-                <li><a href="index.php">Strona Główna</a></li>
-                <li><a href="pracownicy_organizacja.php">Pracownicy i Organizacja</a></li>
-                <li><a href="funkcje_agregujące.php">Funkcję Agregujące</a></li>
-                <li><a href="formularz.html">Formularz</a></li>
-                <li><a href="DaneDoBazy.php">Dane Do Bazy</a></li>
+                <li><a href="pracownicy/https://github.com/AD-2018/sql-php-pierwsza_strona-Kus0202">GitHub</a></li> 
+                <li><a href="pracownicy/index.php">Strona Główna</a></li>
+                <li><a href="pracownicy/pracownicy_organizacja.php">Pracownicy i Organizacja</a></li>
+                <li><a href="pracownicy/funkcje_agregujące.php">Funkcję Agregujące</a></li>
+                <li><a href="pracownicy/formularz.html">Formularz</a></li>
+                <li><a href="pracownicy/DaneDoBazy.php">Dane Do Bazy</a></li>
 </ul>
 </nav>
 
