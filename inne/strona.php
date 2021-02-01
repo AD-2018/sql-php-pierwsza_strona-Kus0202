98f930bb1a5cf7965f1769986bd2533beefc5d05
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-02-01 08:30:59 +0100

    Create strona.php

---
+++ inne/strona.php
@@ -0,0 +1,13 @@
+<?php
+	echo("Jesteś na stronie.php");
+       
+        echo("<ul>");
+
+echo("<li>".$_POST["Firstname"]);
+echo("<li>".$_POST["Lastname"]);
+echo("<li>".$_POST["City"]);
+echo("<li>".$_POST["Phone"]);
+echo("<li>".$_POST["Postcode"]);
+echo("<ul>");
+	
+?>
