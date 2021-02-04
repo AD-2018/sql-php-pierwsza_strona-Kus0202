0fb248c73a6e0e9dd791fc8a189b858599d47d1f
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-02-04 13:50:36 +0100

    Update pracownicy_organizacja.php

--- pracownicy/pracownicy_organizacja.php
+++ pracownicy/pracownicy_organizacja.php
@@ -8,7 +8,7 @@
     <nav>
         <ul>
              <li><a href="https://github.com/AD-2018/sql-php-pierwsza_strona-Kus0202">GitHub</a></li> 
-		<li><a href="../pracownicy/index.php">Strona Główna</a></li>
+		<li><a href="../index.php">Strona Główna</a></li>
                 <li><a href="../pracownicy/funkcje_agregujące.php">Funkcję Agregujące</a></li>
                 <li><a href="../pracownicy/data_czas.php">Data i Czas</a></li>
                 <li><a href="../inne/formularz.html">Formularz</a></li>
