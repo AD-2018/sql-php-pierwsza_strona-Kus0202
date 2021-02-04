5567e2d7c4cb763786c5f32f8683cbb701bc2d89
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-02-04 14:02:22 +0100

    Update DaneDoBazy.php

--- pracownicy/DaneDoBazy.php
+++ pracownicy/DaneDoBazy.php
@@ -16,7 +16,7 @@
 		<li><a href="../pracownicy/pracownicy_organizacja.php">Pracownicy i Organizacja</a></li>
                 <li><a href="../pracownicy/funkcje_agregujące.php">Funkcję Agregujące</a></li>
                 <li><a href="../pracownicy/data_czas.php">Data i Czas</a></li>
-                <li><a href="../pracownicy/formularz.html">Formularz</a></li>
+                <li><a href="../inne/formularz.html">Formularz</a></li>
 		<li><a href="../biblioteka/biblioteka.php">Biblioteka</a></li>
 </ul>
 </nav>
