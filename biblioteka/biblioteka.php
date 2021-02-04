02d8f67c47d2b1468a42256d2c5163f82a4e59f2
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-02-04 13:44:32 +0100

    Update biblioteka.php

--- biblioteka/biblioteka.php
+++ biblioteka/biblioteka.php
@@ -8,12 +8,12 @@
     <nav>
         <ul>
              <li><a href="https://github.com/AD-2018/sql-php-pierwsza_strona-Kus0202">GitHub</a></li> 
-		<li><a href="pracownicy/index.php">Strona Główna</a></li>
-		<li><a href="pracownicy/pracownicy_organizacja.php">Pracownicy i Organizacja</a></li>
-                <li><a href="pracownicy/funkcje_agregujące.php">Funkcję Agregujące</a></li>
-                <li><a href="pracownicy/data_czas.php">Data i Czas</a></li>
-                <li><a href="inne/formularz.html">Formularz</a></li>
-                <li><a href="pracownicy/DaneDoBazy.php">Dane Do Bazy</a></li>
+		<li><a href="../pracownicy/index.php">Strona Główna</a></li>
+		<li><a href="../pracownicy/pracownicy_organizacja.php">Pracownicy i Organizacja</a></li>
+                <li><a href="../pracownicy/funkcje_agregujące.php">Funkcję Agregujące</a></li>
+                <li><a href="../pracownicy/data_czas.php">Data i Czas</a></li>
+                <li><a href="../inne/formularz.html">Formularz</a></li>
+                <li><a href="../pracownicy/DaneDoBazy.php">Dane Do Bazy</a></li>
                 
                 
 </ul>
