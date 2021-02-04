af547003b91321141bd87ba62da34e51c40fb0f6
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-02-04 13:50:49 +0100

    Update funkcje_agregujące.php

--- pracownicy/funkcje_agregujące.php
+++ pracownicy/funkcje_agregujące.php
@@ -11,7 +11,7 @@
     <nav>
         <ul>
                 <li><a href="https://github.com/AD-2018/sql-php-pierwsza_strona-Kus0202">GitHub</a></li> 
-                <li><a href="../pracownicy/index.php">Strona Główna</a></li>
+                <li><a href="../index.php">Strona Główna</a></li>
                 <li><a href="../pracownicy/pracownicy_organizacja.php">Pracownicy i Organizacja</a></li>
                 <li><a href="../pracownicy/data_czas.php">Data i Czas</a></li>
                 <li><a href="../inne/formularz.html">Formularz</a></li>
