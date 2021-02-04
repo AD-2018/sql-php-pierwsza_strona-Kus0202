458e24b8e3722b0e67abf3127e5449c82ddae6b8
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-02-04 13:43:07 +0100

    Update funkcje_agregujące.php

--- pracownicy/funkcje_agregujące.php
+++ pracownicy/funkcje_agregujące.php
@@ -10,13 +10,13 @@
     <h1>Mateusz Kuś nr.20</h1>
     <nav>
         <ul>
-                <li><a href="pracownicy/https://github.com/AD-2018/sql-php-pierwsza_strona-Kus0202">GitHub</a></li> 
-                <li><a href="pracownicy/index.php">Strona Główna</a></li>
-                <li><a href="pracownicy/pracownicy_organizacja.php">Pracownicy i Organizacja</a></li>
-                <li><a href="pracownicy/data_czas.php">Data i Czas</a></li>
-                <li><a href="inne/formularz.html">Formularz</a></li>
-                <li><a href="pracownicy/DaneDoBazy.php">Dane Do Bazy</a></li>
-            <li><a href="biblioteka/biblioteka.php">Biblioteka</a></li>
+                <li><a href="https://github.com/AD-2018/sql-php-pierwsza_strona-Kus0202">GitHub</a></li> 
+                <li><a href="../pracownicy/index.php">Strona Główna</a></li>
+                <li><a href="../pracownicy/pracownicy_organizacja.php">Pracownicy i Organizacja</a></li>
+                <li><a href="../pracownicy/data_czas.php">Data i Czas</a></li>
+                <li><a href="../inne/formularz.html">Formularz</a></li>
+                <li><a href="../pracownicy/DaneDoBazy.php">Dane Do Bazy</a></li>
+            <li><a href="../biblioteka/biblioteka.php">Biblioteka</a></li>
 </ul>
 </nav>
 
