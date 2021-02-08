d69ffd77e9f358f37f6b7675f939f6caef590b10
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-02-08 08:07:40 +0100

    Update bibltytul.php

--- biblioteka/bibltytul.php
+++ biblioteka/bibltytul.php
@@ -1,1 +1,18 @@
+<?php
+echo("bibtytul.php");
+echo "<li>".$_POST['tytul'];
 
+require_once("/connect.php");
+
+$sql = "INSERT INTO bibl_tytul (id_tytul, tytul) 
+       VALUES (null, '".$_POST['tytul']."')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+  header('Location:https://kusmateusz.herokuapp.com/biblioteka/biblioteka.php');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
