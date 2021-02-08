2d85843febefc6414647fc4c89ae413cc27898e4
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-02-08 08:06:43 +0100

    Update biblautor.php

--- biblioteka/biblautor.php
+++ biblioteka/biblautor.php
@@ -10,7 +10,7 @@ $sql = "INSERT INTO biblioteka2 (id_autora, autor)
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
-  header('Location:https://kusmateusz.herokuapp.com/biblioteka/biblautor.php');
+  header('Location:https://kusmateusz.herokuapp.com/biblioteka/biblioteka.php');
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
