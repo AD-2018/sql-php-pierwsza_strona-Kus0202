5647cf5685da3f10c73315ab5b18f088084c5d41
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-02-08 08:05:10 +0100

    Update biblautor.php

--- biblioteka/biblautor.php
+++ biblioteka/biblautor.php
@@ -1,1 +1,19 @@
 
+<?php
+echo("bibautor.php");
+echo "<li>".$_POST['autor'];
+
+require_once("connect.php");
+
+$sql = "INSERT INTO biblioteka2 (id_autora, autor) 
+       VALUES (null, '".$_POST['autor']."')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+  header('Location:https://kusmateusz.herokuapp.com/biblioteka/biblautor.php');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
