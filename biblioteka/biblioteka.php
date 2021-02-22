7a60c8e14cba9bf81327b0864273754a4686bf0a
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-02-22 08:43:54 +0100

    Update biblioteka.php

--- biblioteka/biblioteka.php
+++ biblioteka/biblioteka.php
@@ -19,7 +19,7 @@
 <?php
 require_once ("../connect.php");
 
-$sql = "SELECT autor FROM biblioteka";
+$sql = "SELECT autor FROM biblioteka.sql";
 echo ("<h3>Autorzy</h3>");
 echo ("<li>".$sql);
   $result = mysqli_query($conn, $sql);
@@ -37,7 +37,7 @@ echo('<select name="Autor">');
     }
 echo('</select>');
 
-$sql = "SELECT tytul FROM biblioteka";
+$sql = "SELECT tytul FROM biblioteka.sql";
 echo("<h3>Książki</h3>");
 echo("<li>".$sql."<br><br>");
 
@@ -59,7 +59,7 @@ echo('</select>');
 
 echo ('</table>');
 	
-$sql = "SELECT autor, tytul FROM biblioteka";
+$sql = "SELECT autor, tytul FROM biblioteka.sql";
 echo("<h3>AUTORZY I KSIĄŻKI</h3>");
 echo("<li>".$sql."<br><br>");
 
