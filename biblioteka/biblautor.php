6297efd35cfd9de149f3bfc4ff960a0229244006
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-02-25 12:46:18 +0100

    Update biblautor.php

--- biblioteka/biblautor.php
+++ biblioteka/biblautor.php
@@ -1,1 +1,18 @@
+<?php
+echo("bib_autor.php");
+echo "<li>".$_POST['autor'];
 
+require_once("connect.php");
+
+$sql = "INSERT INTO bibl_autor (id_autora, autor) 
+       VALUES (null, '".$_POST['autor']."')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+  header('Location:https:sql-php-pierwsza_strona-Kus0202/biblioteka.php');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
