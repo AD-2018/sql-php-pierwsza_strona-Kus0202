25dc3d9a4b8cffeceed79b86ef4888c4ac0278d6
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-02-25 13:43:17 +0100

    Update index.php

--- index.php
+++ index.php
@@ -25,6 +25,8 @@
 
   
 <?php
+    $hostname = $_SERVER['HTTP_HOST'];
+    echo("<li> hostname : ".$hostname);
 require_once("connect.php");
 $sql = "SELECT * FROM pracownicy, organizacja WHERE id_org = dzial";
 
