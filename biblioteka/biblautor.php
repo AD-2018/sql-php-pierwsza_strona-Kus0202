2f92efc8f39bf996203ca7183ff876e3c68934c6
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-02-25 12:18:46 +0100

    Update biblautor.php

--- biblioteka/biblautor.php
+++ biblioteka/biblautor.php
@@ -5,7 +5,7 @@ echo "<li>".$_POST['autor'];
 
 require_once("connect.php");
 
-$sql = "INSERT INTO biblioteka2 (id_autora, autor) 
+$sql = "INSERT INTO bibl_autor (id_autora, autor) 
        VALUES (null, '".$_POST['autor']."')";
 
 if ($conn->query($sql) === TRUE) {
