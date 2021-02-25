fb324f0827900365cc01b4d15d624ec4a533e35d
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-02-25 13:04:22 +0100

    Update biblautor.php

--- biblioteka/biblautor.php
+++ biblioteka/biblautor.php
@@ -1,18 +1,29 @@
 <?php
-echo("bibl_autor.php");
-echo "<li>".$_POST['autor'];
+echo("jestes w new_autor.php <br>");
+echo "<li>". $_POST['autor'];
 
-require_once("connect.php");
+$servername = "mysql-mateusz.alwaysdata.net";
+    $username = "mateusz";
+    $password = "Strona123";
+    $dbname = "mateusz_kus";
 
-$sql = "INSERT INTO bibl_autor (id_autora, autor) 
-       VALUES (null, '".$_POST['autor']."')";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+//definiujemy zapytanie $sql
+$sql = "INSERT INTO bibl_autor (id_autor,autor) 
+      VALUES (null, '".$_POST['autor']."')";
+
+//wyświetlamy zapytanie $sql
+echo "<li>". $sql;
 
 if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-  header('Location:https://kusmateusz.herokuapp.com/biblioteka/biblioteka.php');
+   header ('Location:https://kusmateusz.herokuapp.com/biblioteka/biblioteka.php');
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
 $conn->close();
-?>
+?> 
