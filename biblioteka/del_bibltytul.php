07f68766e3375f252edd0682dd69346ec8e480d1
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-02-25 13:15:34 +0100

    Update del_bibltytul.php

--- biblioteka/del_bibltytul.php
+++ biblioteka/del_bibltytul.php
@@ -1,1 +1,28 @@
+<?php
+echo("Delete");
+echo $_POST['id_tytul'];
 
+$servername = "mysql-mateusz.alwaysdata.net";
+    $username = "mateusz";
+    $password = "Strona123";
+    $dbname = "mateusz_kus";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "DELETE FROM bibl_tytul WHERE id_tytul=".$_POST['id_tytul'];
+
+echo $sql;
+
+if ($conn->query($sql) === TRUE) {
+    header ('Location:https://kusmateusz.herokuapp.com/biblioteka/biblioteka.php');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
