7f950903fee0f2c791e34ebdebae27d3b5831239
Mateusz <kusmateusz31@gmail.com>
2021-03-17 22:16:44 +0100

    DATAA

--- pracownicy/pracownicy_organizacja.php
+++ pracownicy/pracownicy_organizacja.php
@@ -21,7 +21,10 @@
 <?php 
 require_once("connect.php");
 
+$data=date("Y-m-d");
+$czas=date("H:i");
 
+echo "Stronę wyświetlono dnia $data o godzinie $czas";
 
 	echo ('<h2>Pracownicy i Organizacja</h2>');
     echo ('<h3>Lista Pracowników<h3>');
