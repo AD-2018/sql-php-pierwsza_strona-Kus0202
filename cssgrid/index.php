8fea4876ef23b70d7943df310091446b64ff5a46
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-03-21 16:19:50 +0100

    Update index.php

--- cssgrid/index.php
+++ cssgrid/index.php
@@ -14,6 +14,10 @@
 </div>
     <div class = "nav">
       <a class="panel" href="/cssgrid/Pierwsza Stronka/grid.php">Pierwsza Stronka</a>
+        <a class="panel" href="/cssgrid/Druga Stronka/grid.php">Druga Stronka</a>
+        <a class="panel" href="/cssgrid/Trzecia Stronka/grid.php">Trzecia Stronka</a>
+        <a class="panel" href="/cssgrid/Czwarta Stronka/grid.php">Czwarta Stronka</a>
+        <a class="panel" href="/cssgrid/Piąta Stronka/grid.php">Piąta Stronka</a>
         
     </div>
     </body>
