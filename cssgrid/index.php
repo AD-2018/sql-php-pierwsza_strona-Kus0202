556bebe5e7c2d8c955e6209b281355b020d763a4
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-03-21 16:06:01 +0100

    Update index.php

--- cssgrid/index.php
+++ cssgrid/index.php
@@ -6,16 +6,11 @@
 </head>
 <body>
 <div class= "imie">    
-<a href="https://github.com/AD-2018/sql-php-pierwsza_strona-DanielHarazim">github</a>
-<h1>Daniel Harazim</h1>
+<a href="https://github.com/AD-2018/sql-php-pierwsza_strona-Kus0202">GitHub</a>
+<h1>Mateusz Kuś</h1>
 </div>
     <div class = "nav">
         <a class="panel" href="/index.php">Wróć</a>
-        <a class="panel" href="/cssgrid/Pierwsza Stronka/grid.php">Pierwsza Stronka</a>
-        <a class="panel" href="/cssgrid/strona2/grid.php">STRONA 2</a>
-        <a class="panel" href="/cssgrid/strona3/grid.php">STRONA 3</a>
-        <a class="panel" href="/cssgrid/strona4/grid.php">STRONA 4</a>
-        <a class="panel" href="/cssgrid/strona5/grid.php">STRONA 5</a>
         
     </div>
     </body>
