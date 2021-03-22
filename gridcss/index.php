09eb2193fbb0b21a264c44bc731086c4e3333453
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-03-22 09:09:25 +0100

    Update index.php

--- gridcss/index.php
+++ gridcss/index.php
@@ -1,1 +1,21 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="/style.css">
+<title>GRID</title>
+</head>
+<body>
+<div class= "imie">    
+<a href="https://github.com/AD-2018/sql-php-pierwsza_strona-Kus0202">GitHub</a>
+<h1>Mateusz Kuś</h1>
+</div>
+    <div class = "nav">
+        <a class="panel" href="/cssgrid/Pierwsza Stronka/grid.php">Pierwsza Stronka</a>
+        <a class="panel" href="/cssgrid/Druga Stronka/grid.php">Druga Stronka</a>
+        <a class="panel" href="/cssgrid/Trzecia Stronka/grid.php">Trzecia Stronka</a>
+        <a class="panel" href="/cssgrid/Czwarta Stronka/grid.php">Czwarta Stronka</a>
+        <a class="panel" href="/cssgrid/Piąta Stronka/grid.php">Piąta Stronka</a>
+        
+    </div>
+    </body>
+    </html>
