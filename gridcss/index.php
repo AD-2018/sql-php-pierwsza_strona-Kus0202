a9f07d0014cbc3feefd68258545e882a1beb9a52
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-03-22 09:12:21 +0100

    Update index.php

--- gridcss/index.php
+++ gridcss/index.php
@@ -10,11 +10,11 @@
 <h1>Mateusz Kuś</h1>
 </div>
     <div class = "nav">
-        <a class="panel" href="/cssgrid/Pierwsza Stronka/grid.php">Pierwsza Stronka</a>
-        <a class="panel" href="/cssgrid/Druga Stronka/grid.php">Druga Stronka</a>
-        <a class="panel" href="/cssgrid/Trzecia Stronka/grid.php">Trzecia Stronka</a>
-        <a class="panel" href="/cssgrid/Czwarta Stronka/grid.php">Czwarta Stronka</a>
-        <a class="panel" href="/cssgrid/Piąta Stronka/grid.php">Piąta Stronka</a>
+        <a class="panel" href="/gridcss/Pierwsza Stronka/grid.php">Pierwsza Stronka</a>
+        <a class="panel" href="/gridcss/Druga Stronka/grid.php">Druga Stronka</a>
+        <a class="panel" href="/gridcss/Trzecia Stronka/grid.php">Trzecia Stronka</a>
+        <a class="panel" href="/gridcss/Czwarta Stronka/grid.php">Czwarta Stronka</a>
+        <a class="panel" href="/gridcss/Piąta Stronka/grid.php">Piąta Stronka</a>
         
     </div>
     </body>
