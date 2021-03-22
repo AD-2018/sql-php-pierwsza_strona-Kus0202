ecee558bbd684aa327a397aa6a2adf455022e27b
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-03-22 08:28:55 +0100

    Update grid.php

--- gridcss/Pierwsza Stronka/grid.php
+++ gridcss/Pierwsza Stronka/grid.php
@@ -1,1 +1,31 @@
-
+<html>
+<head>
+<link rel="stylesheet" href="grid.css">
+	<title>Mateusz Kus</title>
+</head>
+<body>
+    <h1>Mateusz Kus nr.20</h1>
+    <nav>
+        <ul>
+             <li><a href="https://github.com/AD-2018/sql-php-pierwsza_strona-Kus0202">GitHub</a></li> 
+		<li><a href="../index.php">Strona Głowna</a></li>
+            
+<div class="container">
+    <header>
+        <h1>Wprowadzenie do Grid'a </h1>
+    </header>
+    <nav>
+    Pierwsza Stronka
+    </nav>
+    <main>
+    Druga Stronka
+    </main>
+    <aside>
+    Trzecia Stronka
+    </aside>
+    <footer>
+    Czwarta Stronka
+    </footer>
+   </div>
+</body>
+</html>
