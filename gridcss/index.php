291048cf9a94851c83b9f03c45f89121aff89e40
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-03-28 23:43:45 +0200

    Update index.php

--- gridcss/index.php
+++ gridcss/index.php
@@ -11,17 +11,17 @@
 </div>
     <div class = "nav">
         <a class="panel" href="/gridcss/Pierwsza Stronka/grid.html">Pierwsza Stronka</a>
-        <a class="panel" href="/gridcss/Druga Stronka/grid.php">Druga Stronka</a>
-        <a class="panel" href="/gridcss/Trzecia Stronka/grid.php">Trzecia Stronka</a>
-        <a class="panel" href="/gridcss/Czwarta Stronka/grid.php">Czwarta Stronka</a>
-        <a class="panel" href="/gridcss/Piąta Stronka/grid.php">Piąta Stronka</a>
-         <a class="panel" href="/gridcss/Szósta Stronka/grid.php">Szósta Stronka</a>
-         <a class="panel" href="/gridcss/Siódma Stronka/grid.php">Siódma Stronka</a>
-         <a class="panel" href="/gridcss/Ósma Stronka/grid.php">Ósma Stronka</a>
-         <a class="panel" href="/gridcss/Dziewiąta Stronka/grid.php">Dziewiąta Stronka</a>
-         <a class="panel" href="/gridcss/Dziesiąta Stronka/grid.php">Dziesiąta Stronka</a>
-         <a class="panel" href="/gridcss/Jedenasta Stronka/grid.php">Jedenasta Stronka</a>
-         <a class="panel" href="/gridcss/Dwunasta Stronka/grid.php">Dwunasta Stronka</a>
+        <a class="panel" href="/gridcss/Druga Stronka/grid.html">Druga Stronka</a>
+        <a class="panel" href="/gridcss/Trzecia Stronka/grid.html">Trzecia Stronka</a>
+        <a class="panel" href="/gridcss/Czwarta Stronka/grid.html">Czwarta Stronka</a>
+        <a class="panel" href="/gridcss/Piąta Stronka/grid.html">Piąta Stronka</a>
+         <a class="panel" href="/gridcss/Szósta Stronka/grid.html">Szósta Stronka</a>
+         <a class="panel" href="/gridcss/Siódma Stronka/grid.html">Siódma Stronka</a>
+         <a class="panel" href="/gridcss/Ósma Stronka/grid.html">Ósma Stronka</a>
+         <a class="panel" href="/gridcss/Dziewiąta Stronka/grid.html">Dziewiąta Stronka</a>
+         <a class="panel" href="/gridcss/Dziesiąta Stronka/grid.html">Dziesiąta Stronka</a>
+         <a class="panel" href="/gridcss/Jedenasta Stronka/grid.html">Jedenasta Stronka</a>
+         <a class="panel" href="/gridcss/Dwunasta Stronka/grid.html">Dwunasta Stronka</a>
         
     </div>
     </body>
