3ad463b5699e1f17312c9aa0c1352babfb7938ae
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-03-28 10:50:11 +0200

    Update grid.php

--- gridcss/Szósta Stronka/grid.php
+++ gridcss/Szósta Stronka/grid.php
@@ -1,1 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Mateusz Kuś</title>
+        <link rel="stylesheet" href="grid.css"/>
+    </head>
+    <body>
+        <div class="container">
+            <header>
+                <h1>CSS GRID</h1>
+            </header>
+            <nav>
+                1
+            </nav>
+            <main>
+                2
+            </main>
+            <aside>
+                4
+            </aside>
+            <footer>
+                3
+            </footer>
+            <div class= "zolte">
+                5
+            </div>
+        </div>
+    </body>
+</html>
 
