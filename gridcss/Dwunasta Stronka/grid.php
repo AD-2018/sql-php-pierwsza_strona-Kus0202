68a9d29ac069346e2f788e5545f7b9ea6c60c2ea
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-03-28 11:05:02 +0200

    Update grid.php

--- gridcss/Dwunasta Stronka/grid.php
+++ gridcss/Dwunasta Stronka/grid.php
@@ -1,1 +1,41 @@
-
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Szymon Kozak</title>
+        <link rel="stylesheet" href="style_1.css"/>
+    </head>
+    <body>
+        <div class="container">
+            <header>
+                <h1>CSS GRID</h1>
+            </header>
+            <nav>
+                1
+            </nav>
+            <main>
+                2
+            </main>
+            <aside>
+                4
+            </aside>
+            <footer>
+                3
+            </footer>
+            <div class="a">
+                5
+            </div>
+            <div class="b">
+                6
+            </div>
+            <div class="c">
+                7
+            </div>
+            <div class="d">
+                8
+            </div>
+            <div class="e">
+                9
+            </div>
+        </div>
+    </body>
+</html>
