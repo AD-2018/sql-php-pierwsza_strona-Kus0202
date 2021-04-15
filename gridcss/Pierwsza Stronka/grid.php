2e474684e6fcaf8f71db1e98af43b8b8121946c2
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-04-15 10:48:02 +0200

    Update grid.php

--- gridcss/Pierwsza Stronka/grid.php
+++ gridcss/Pierwsza Stronka/grid.php
@@ -10,21 +10,82 @@
    <body>
     
                                          <div class="container">
-      <header>
-                         <h1>CSS GRID</h1>
+     <header>
+      <div class="tabela1">
+      <?php
+    require_once("../../connect.php");
+    $sql = "SELECT * FROM nauczyciele";
+    echo("<br>");
+    echo($sql);
+    $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+        } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+     }
+    echo('<table border="1">');
+        echo('<th>id</th><th>Nauczyciel</th>');
+        while($row=mysqli_fetch_assoc($result)){
+            echo('<tr>');
+            echo('<td>'.$row['id'].'</td><td>'.$row['Nauczyciel'].'</td>');
+            echo('</tr>');
+     }
+        echo('</table>');
+?>
+        </div>
       </header>
       <nav>
-        2
+      2
       </nav>
       <main>
-        3
+      <?php
+    require_once("../../connect.php");
+    $sql = "SELECT szkola.id, Nauczyciel, nazwisko FROM nauczyciele, szkola, Uczen where nauczyciele.id=szkola.nauczyciele_id and Uczen.id=szkola.uczen_id";
+    echo("<br>");
+    echo($sql);
+    $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+        } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+     }
+    echo('<table border="1">');
+        echo('<th>id</th><th>Nauczyciel</th><th>Uczeń</th>');
+        while($row=mysqli_fetch_assoc($result)){
+            echo('<tr>');
+            echo('<td>'.$row['id'].'</td><td>'.$row['nazwisko'].'</td><td>'.$row['Nauczyciel'].'</td>');
+            echo('</tr>');
+     }
+        echo('</table>');
+?>
       </main>
-      <aside>
-        5
-        </aside>
       <footer>
-        4
+      4
       </footer>
+      <aside>
+      <div class="tabela1">
+      <?php
+    require_once("../../connect.php");
+    $sql = "SELECT * FROM Uczen";
+    echo("<br>");
+    echo($sql);
+    $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+        } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+     }
+    echo('<table border="1">');
+        echo('<th>Id</th><th>nazwisko</th>');
+        while($row=mysqli_fetch_assoc($result)){
+            echo('<tr>');
+            echo('<td>'.$row['id'].'</td><td>'.$row['nazwisko'].'</td>');
+            echo('</tr>');
+     }
+        echo('</table>');
+?>
+        </div>
+      </aside>
     </div>
   </body>
 </html>
