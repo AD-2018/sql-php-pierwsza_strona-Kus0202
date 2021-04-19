edb2a0b59539d4381d89a99fe8eed00055a1ab40
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-04-19 08:32:29 +0200

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,1 +1,8 @@
-
+                <li><a href="https://github.com/AD-2018/sql-php-pierwsza_strona-Kus0202">GitHub</a></li> 
+                <li><a href="pracownicy/pracownicy_organizacja.php">Pracownicy i Organizacja</a></li>
+                <li><a href="pracownicy/funkcje_agregujące.php">Funkcję Agregujące</a></li>
+                <li><a href="pracownicy/data_czas.php">Data i Czas</a></li>
+                <li><a href="inne/formularz.html">Formularz</a></li>
+                <li><a href="pracownicy/DaneDoBazy.php">Dane Do Bazy</a></li>
+                <li><a href="biblioteka/biblioteka.php">Biblioteka</a></li>
+                <li><a href="gridcss/index.php">Grid</a></li>
