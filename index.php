debc839c7669e855b4f82b854629aa102e370ccd
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-04-19 08:37:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,18 +10,7 @@
     <h1>Mateusz Kuś nr.20</h1>
     <nav>
         <ul>
-            <?php include_once("menu.php"); ?>
-             <li><a href="https://github.com/AD-2018/sql-php-pierwsza_strona-Kus0202">GitHub</a></li> 
-                <li><a href="pracownicy/pracownicy_organizacja.php">Pracownicy i Organizacja</a></li>
-                <li><a href="pracownicy/funkcje_agregujące.php">Funkcję Agregujące</a></li>
-                <li><a href="pracownicy/data_czas.php">Data i Czas</a></li>
-                <li><a href="inne/formularz.html">Formularz</a></li>
-                <li><a href="pracownicy/DaneDoBazy.php">Dane Do Bazy</a></li>
-            <li><a href="biblioteka/biblioteka.php">Biblioteka</a></li>
-            <li><a href="gridcss/index.php">Grid</a></li>
-          
-                
-                
+            <?php include_once("menu.php"); ?>       
 </ul>
 </nav>
 
