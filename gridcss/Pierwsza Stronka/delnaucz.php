0accccf6decab2e6fd2e3ab5bd6ef52ffcf0b75b
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-04-22 12:03:13 +0200

    Create delnaucz.php

---
+++ gridcss/Pierwsza Stronka/delnaucz.php
@@ -0,0 +1,20 @@
+<?php
+echo("Jesteś w delete.php <br>");
+echo $_POST['id'];
+
+require_once("../../connect.php");
+
+
+
+$sql = "DELETE FROM Nauczyciele WHERE id=".$_POST['id'];
+
+echo $sql;
+
+if ($conn->query($sql) === TRUE) {
+    header ('Location:https://kusmateusz.herokuapp.com/gridcss/Pierwsza%20Stronka/grid.php');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
