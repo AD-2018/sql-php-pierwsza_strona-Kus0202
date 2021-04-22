e1c99b17241be4c71eb4c7e33f399301e824e6b5
Mateusz Kuś <73634980+kusmateusz@users.noreply.github.com>
2021-04-22 10:13:56 +0200

    Update delete.php

--- gridcss/delete.php
+++ gridcss/delete.php
@@ -11,7 +11,7 @@ $sql = "DELETE FROM Nauczyciele WHERE id_naucz=".$_POST['id'];
 echo $sql;
 
 if ($conn->query($sql) === TRUE) {
-    header ('Location:https://kusmateusz.herokuapp.com/gridcss/Pierwsza Stronka/grid.php');
+    header ('Location:https://kusmateusz.herokuapp.com/gridcss/Pierwsza%20Stronka/grid.php');
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
